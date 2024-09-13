c02239bbff2f0964dac47affd2878e629657ceb4
Imam Abdillah <92290516+imamabdillah@users.noreply.github.com>
2024-09-13 17:37:33 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -29,16 +29,17 @@ export default function Home() {
         quantity={100}
       />
       <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-6xl whitespace-nowrap bg-clip-text ">
-        Imam Abdillah
+        Imam Abdillah Purwanto
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-        "I am a Fullstack Web Developer with expertise in both frontend and backend development, creating engaging and functional user experiences for web applications."
+          "I am a Fullstack Web Developer with expertise in both frontend and
+          backend development, creating engaging and functional user experiences
+          for web applications."
         </h2>
       </div>
     </div>
   );
-
 }
