3588ad9f0e15300f09ba5f737aa7974895703525
Imam Abdillah <92290516+imamabdillah@users.noreply.github.com>
2024-11-28 17:26:07 +0700

    up

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -8,10 +8,20 @@ import { Eye } from "lucide-react";
 
 export default function ProjectsPage() {
   // Retrieve the featured, top2, and top3 projects
-  const featured = allProjects.find((project) => project.slug === "kasir-foodcourt")!;
-  const top2 = allProjects.find((project) => project.slug === "desa-cantik")!;
-  const top3 = allProjects.find((project) => project.slug === "voting")!;
-  const onlineVoting = allProjects.find((project) => project.slug === "web-phone-store")!;
+  const featured = allProjects.find(
+    (project) => project.slug === "e-procurement"
+  )!;
+  const top2 = allProjects.find(
+    (project) => project.slug === "kasir-foodcourt"
+  )!;
+  const top3 = allProjects.find((project) => project.slug === "desa-cantik")!;
+
+  const onlineVoting = allProjects.find(
+    (project) => project.slug === "voting"
+  )!;
+  const phoneStore = allProjects.find(
+    (project) => project.slug === "web-phone-store"
+  );
   const skd = allProjects.find((project) => project.slug === "skd-uas")!;
 
   // Sort the remaining projects
@@ -22,14 +32,14 @@ export default function ProjectsPage() {
         project.slug !== featured.slug &&
         project.slug !== top2.slug &&
         project.slug !== top3.slug &&
-        project.slug !== onlineVoting.slug && 
-        project.slug !== skd.slug, 
-
+        project.slug !== onlineVoting.slug &&
+        project.slug !== phoneStore.slug &&
+        project.slug !== skd.slug
     )
     .sort(
       (a, b) =>
         new Date(b.date ?? Number.POSITIVE_INFINITY).getTime() -
-        new Date(a.date ?? Number.POSITIVE_INFINITY).getTime(),
+        new Date(a.date ?? Number.POSITIVE_INFINITY).getTime()
     );
 
   return (
@@ -90,7 +100,58 @@ export default function ProjectsPage() {
 
           {/* Top 2 and Top 3 projects */}
           <div className="flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0">
-            {[top2, top3].map((project) => (
+            {[top2, top3].map(
+              (project) =>
+                project && (
+                  <Card key={project.slug}>
+                    <Link href={`/projects/${project.slug}`}>
+                      <article className="relative w-full h-full p-4 md:p-8">
+                        <div className="flex items-center justify-between gap-2">
+                          <div className="text-xs text-zinc-100">
+                            {project.date ? (
+                              <time
+                                dateTime={new Date(project.date).toISOString()}
+                              >
+                                {Intl.DateTimeFormat(undefined, {
+                                  dateStyle: "medium",
+                                }).format(new Date(project.date))}
+                              </time>
+                            ) : (
+                              <span>SOON</span>
+                            )}
+                          </div>
+                          <span className="flex items-center gap-1 text-xs text-zinc-500">
+                            <Eye className="w-4 h-4" />{" "}
+                            {Intl.NumberFormat("en-US", {
+                              notation: "compact",
+                            }).format(
+                              0 // Replace with actual views data
+                            )}
+                          </span>
+                        </div>
+                        <h2 className="mt-4 text-3xl font-bold text-zinc-100 group-hover:text-white sm:text-4xl font-display">
+                          {project.title}
+                        </h2>
+                        <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
+                          {project.description}
+                        </p>
+                        <div className="static bottom-4 md:bottom-8">
+                          <p className="hidden text-zinc-200 hover:text-zinc-50 lg:block">
+                            Read more <span aria-hidden="true">&rarr;</span>
+                          </p>
+                        </div>
+                      </article>
+                    </Link>
+                  </Card>
+                )
+            )}
+          </div>
+        </div>
+
+        {/* Display Web Vote project and other projects in a grid */}
+        <div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3">
+          {[onlineVoting, phoneStore, skd, ...sorted].map(
+            (project) =>
               project && (
                 <Card key={project.slug}>
                   <Link href={`/projects/${project.slug}`}>
@@ -98,7 +159,9 @@ export default function ProjectsPage() {
                       <div className="flex items-center justify-between gap-2">
                         <div className="text-xs text-zinc-100">
                           {project.date ? (
-                            <time dateTime={new Date(project.date).toISOString()}>
+                            <time
+                              dateTime={new Date(project.date).toISOString()}
+                            >
                               {Intl.DateTimeFormat(undefined, {
                                 dateStyle: "medium",
                               }).format(new Date(project.date))}
@@ -109,7 +172,9 @@ export default function ProjectsPage() {
                         </div>
                         <span className="flex items-center gap-1 text-xs text-zinc-500">
                           <Eye className="w-4 h-4" />{" "}
-                          {Intl.NumberFormat("en-US", { notation: "compact" }).format(
+                          {Intl.NumberFormat("en-US", {
+                            notation: "compact",
+                          }).format(
                             0 // Replace with actual views data
                           )}
                         </span>
@@ -129,52 +194,7 @@ export default function ProjectsPage() {
                   </Link>
                 </Card>
               )
-            ))}
-          </div>
-        </div>
-
-        {/* Display Web Vote project and other projects in a grid */}
-        <div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3">
-          {[skd,onlineVoting, ...sorted].map((project) => (
-            project && (
-              <Card key={project.slug}>
-                <Link href={`/projects/${project.slug}`}>
-                  <article className="relative w-full h-full p-4 md:p-8">
-                    <div className="flex items-center justify-between gap-2">
-                      <div className="text-xs text-zinc-100">
-                        {project.date ? (
-                          <time dateTime={new Date(project.date).toISOString()}>
-                            {Intl.DateTimeFormat(undefined, {
-                              dateStyle: "medium",
-                            }).format(new Date(project.date))}
-                          </time>
-                        ) : (
-                          <span>SOON</span>
-                        )}
-                      </div>
-                      <span className="flex items-center gap-1 text-xs text-zinc-500">
-                        <Eye className="w-4 h-4" />{" "}
-                        {Intl.NumberFormat("en-US", { notation: "compact" }).format(
-                          0 // Replace with actual views data
-                        )}
-                      </span>
-                    </div>
-                    <h2 className="mt-4 text-3xl font-bold text-zinc-100 group-hover:text-white sm:text-4xl font-display">
-                      {project.title}
-                    </h2>
-                    <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
-                      {project.description}
-                    </p>
-                    <div className="static bottom-4 md:bottom-8">
-                      <p className="hidden text-zinc-200 hover:text-zinc-50 lg:block">
-                        Read more <span aria-hidden="true">&rarr;</span>
-                      </p>
-                    </div>
-                  </article>
-                </Link>
-              </Card>
-            )
-          ))}
+          )}
         </div>
       </div>
     </div>
